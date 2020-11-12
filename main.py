e71bd893d0fe0b8a2563c10dd93f2c5371fc6333
nikolska <nikolska1995@gmail.com>
2020-11-12 15:39:44 +0100

    doc-str

--- main.py
+++ main.py
@@ -5,6 +5,20 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def guess_the_number():
+    '''
+    Function guess_the_number checks the expected user number, replacing the max or min limits
+    of the possibility of finding the number.
+
+    Function returns a new calculated value of a possible number and selection buttons until
+    the number guessed by the user is guessed by the computer.
+
+    :welcome_text type: html-file
+    :options type: html-file
+    :answer: the user's answer, selectable by pressing one of the buttons, type - string
+    :guessing: computer number selection algorithm, type - int
+    :min_num: minimum border, type - int
+    :max_num: maximum border, type - int
+    '''
     if request.method == 'GET':
         welcome_text = render_template('welcome.html')
         return welcome_text
